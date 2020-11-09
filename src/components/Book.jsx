55f54fb078bd4fc9f88db09a9cbf2256475e98ae
isabelxklee <hello@isabelklee.co>
2020-11-09 12:16:51 -0500

    created toggle panel to show book details

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,21 +1,38 @@
 import React, {Component} from 'react'
+import DetailPanel from './DetailPanel.jsx'
 
 class Book extends Component {
+  state = {
+    showPanel: false
+  }
+  
+  handleClick = () => {
+    this.setState({
+      showPanel: !this.state.showPanel
+    })
+  }
+
   render () {
-    let {id, author, title, description, image, published} = this.props.book
+    let {id, author, title, image} = this.props.book
+    console.log(this.state.showPanel)
 
     return (
       <>
         {id % 2 === 0
         ?
           <div className="book" id="top">
-            <img src={image} alt={`Book cover for ${title} by ${author}`}/>
+            <img src={image} alt={`Book cover for ${title} by ${author}`} onClick={this.handleClick}/>
           </div>
         :
           <div className="book" id="bottom">
             <img src={image} alt={`Book cover for ${title} by ${author}`}/>
           </div>
         }
+        {this.state.showPanel === true
+        ?
+          <DetailPanel/>
+        :
+          null}
       </>
     )
   }
---
+++ src/components/DetailPanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DetailPanel(props) {
+  return (
+    <div className="panel">
+      <h1>hello world</h1>
+    </div>
+  )
+}
+
+export default DetailPanel
