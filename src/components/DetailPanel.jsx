e65325632cea964e2687ef271e1656c2f32b0411
isabelxklee <hello@isabelklee.co>
2021-01-11 12:06:26 -0500

    fixed panel scrolling

--- src/components/DetailPanel.jsx
+++ src/components/DetailPanel.jsx
@@ -4,14 +4,20 @@ const DetailPanel = ({book}) => {
   return (
     <aside className="side-panel">
       {book ? (
-        <>
+        <section className="featured-book-info">
           <h1 className="panel-title">{book.title}</h1>
+          <img
+            src={book.image}
+            alt={`Book cover for ${book.title} by ${book.author}`}
+          />
           <h3>By {book.author}</h3>
           <p>{book.description}</p>
-          <hr />
-          <p>Published in {book.published}</p>
-        </>
-      ) : null}
+        </section>
+      ) :
+      <section className="empty-book">
+        <p>Pssst... select a book from the left!</p>
+      </section>
+      }
     </aside>
   )
 }
