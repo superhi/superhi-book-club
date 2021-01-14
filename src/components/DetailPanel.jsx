043d24a87739d551f0b7392ca3adaf069fc3cef3
isabelxklee <hello@isabelklee.co>
2021-01-14 14:56:27 -0500

    created new div for book info

--- src/components/DetailPanel.jsx
+++ src/components/DetailPanel.jsx
@@ -10,10 +10,12 @@ const DetailPanel = ({book, closePanel}) => (
         alt={`Book cover for ${book.title} by ${book.author}`}
         className="book featured"
       />
-      <h2 className="panel-title">{book.title}</h2>
-      <p className="author">By {book.author}</p>
-      <p className="description">{book.description}</p>
-      <p className="date">Published in {book.published}</p>
+      <div className="book-info">
+        <h2 className="book-title">{book.title}</h2>
+        <p className="author featured">By {book.author}</p>
+        <p className="description featured">{book.description}</p>
+        <p className="date featured">Published in {book.published}</p>
+      </div>
     </div>
   </aside>
 )
