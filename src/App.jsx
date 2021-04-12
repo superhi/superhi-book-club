9e3ca684c44768c627cf92fd0f45c7127e947465
isabelxklee <hello@isabelklee.co>
2021-04-12 19:18:38 -0400

    added books container and book component

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,22 @@
 import React, {useState, useEffect} from 'react'
-import {GlobalStyle} from './styles'
 import BooksContainer from './components/BooksContainer'
-import Header from './components/Header'
-import DetailPanel from './components/DetailPanel'
-import Search from './components/Search'
 
 const App = () => {
   const [books, setBooks] = useState([])
-  const [filteredBooks, setFilteredBooks] = useState([])
-  const [selectedBook, setSelectedBook] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch('https://book-club-json.herokuapp.com/books')
       const books = await response.json()
       setBooks(books)
-      setFilteredBooks(books)
     }
 
     fetchData()
   }, [])
 
-  const pickBook = (book) => {
-    setSelectedBook(book)
-  }
-
-  const closePanel = () => {
-    setSelectedBook(null)
-  }
-
-  const filterBooks = (searchTerm) => {
-    const stringSearch = (bookAttribute, searchTerm) =>
-      bookAttribute.toLowerCase().includes(searchTerm.toLowerCase())
-
-    if (!searchTerm) {
-      setFilteredBooks(books)
-    } else {
-      setFilteredBooks(
-        books.filter(
-          (book) => stringSearch(book.title, searchTerm) || stringSearch(book.author, searchTerm)
-        )
-      )
-    }
-  }
-
-  const hasFiltered = filteredBooks.length !== books.length
-
   return (
     <>
-      <GlobalStyle />
-      <Header>
-        <Search filterBooks={filterBooks} />
-      </Header>
-      <BooksContainer
-        books={filteredBooks}
-        pickBook={pickBook}
-        isPanelOpen={selectedBook !== null}
-        title={hasFiltered ? 'Search results' : 'All books'}
-      />
-      {selectedBook && <DetailPanel book={selectedBook} closePanel={closePanel} />}
+      <BooksContainer books={books}/>
     </>
   )
 }
--- src/components/BooksContainer/index.jsx
+++ src/components/BooksContainer/index.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Book from '../Book'
 import {Container, H2, BookList} from './styles'
 
-const BooksContainer = ({books, pickBook, title, isPanelOpen}) => (
-  <Container $isPanelOpen={isPanelOpen}>
-    <H2>{title}</H2>
+const BooksContainer = ({books}) => (
+  <Container>
+    <H2>All books</H2>
     <BookList>
       {books.map((book) => (
-        <Book key={book.id} book={book} pickBook={pickBook} />
+        <Book key={book.id} book={book} />
       ))}
     </BookList>
   </Container>
