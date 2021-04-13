9171f94210c7aba46fbfe33e8abc3734b8622c94
isabelxklee <hello@isabelklee.co>
2021-04-13 17:23:36 -0400

    removed old exit icon

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,33 +1,43 @@
-import React, {useState, useRef} from 'react'
-import {Input, SearchContainer, Icon, Wrapper} from './styles'
+import React from 'react'
+import {Input, SearchContainer, Icon} from './styles'
 import {Close} from '../../styles'
 
 const Search = ({filterBooks}) => {
-  const inputEl = useRef(null)
-  const [showOnDesktop, setShowOnDesktop] = useState(false)
+  // const inputEl = useRef(null)
+  // const [showOnDesktop, setShowOnDesktop] = useState(false)
 
   const handleChange = (event) => {
     filterBooks(event.target.value)
   }
 
-  const showSearch = () => {
-    setShowOnDesktop(true)
-  }
+  // const showSearch = () => {
+  //   setShowOnDesktop(true)
+  // }
 
-  const clearSearch = () => {
-    filterBooks('')
-    setShowOnDesktop(false)
-    inputEl.current.value = ''
-  }
+  // const clearSearch = () => {
+  //   filterBooks('')
+  //   setShowOnDesktop(false)
+  //   inputEl.current.value = ''
+  // }
 
   return (
-    <Wrapper>
-      <SearchContainer $showOnDesktop={showOnDesktop}>
-        <Icon onClick={showSearch} />
-        <Input ref={inputEl} type="text" name="search" onChange={handleChange} autoComplete="off" />
-        <Close onClick={clearSearch} />
-      </SearchContainer>
-    </Wrapper>
+    <SearchContainer
+    // $showOnDesktop={showOnDesktop}
+    >
+      <Icon
+      // onClick={showSearch}
+      />
+      <Input
+        // ref={inputEl}
+        type="text"
+        name="search"
+        onChange={handleChange}
+        autoComplete="off"
+      />
+      <Close
+      // onClick={clearSearch}
+      />
+    </SearchContainer>
   )
 }
 
--- src/components/Search/styles.js
+++ src/components/Search/styles.js
@@ -2,38 +2,15 @@ import styled from 'styled-components'
 import {ReactComponent as MagnifyingIcon} from '../../assets/search.svg'
 import {Pill} from '../../styles'
 
-export const Wrapper = styled.div`
-  @media (max-width: 800px) {
-    border-top: 2px solid black;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: #ffbccc;
-    height: 64px;
-    width: 100vw;
-    left: 0;
-    bottom: 0;
-    position: fixed;
-    z-index: 2;
-  }
-`
-
 export const SearchContainer = styled(Pill)`
   transition: 0.3s;
-  width: ${({$showOnDesktop}) => ($showOnDesktop ? '420px' : '20px')};
+  width: 420px;
+  /* width: ${({$showOnDesktop}) => ($showOnDesktop ? '420px' : '20px')}; */
 
-  input,
+  /* input,
   button {
     display: ${({$showOnDesktop}) => ($showOnDesktop ? 'block' : 'none')};
-
-    @media (max-width: 800px) {
-      display: block;
-    }
-  }
-
-  @media (max-width: 800px) {
-    width: 85%;
-  }
+  } */
 `
 
 export const Input = styled.input`
