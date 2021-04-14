955840a527730789f5ac31eaf08916be327d1e81
Ryan <ryan@artcommaco.de>
2021-04-14 13:26:17 +0100

    fix for search on mobile

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,7 +2,7 @@ import React, {useState, useRef} from 'react'
 import {Input, SearchContainer, Icon, Wrapper} from './styles'
 import {Close} from '../../styles'
 
-const Search = ({filterBooks, isPanelOpen}) => {
+const Search = ({filterBooks}) => {
   const inputEl = useRef(null)
   const [showOnDesktop, setShowOnDesktop] = useState(false)
 
@@ -21,7 +21,7 @@ const Search = ({filterBooks, isPanelOpen}) => {
   }
 
   return (
-    <Wrapper $isPanelOpen={isPanelOpen}>
+    <Wrapper>
       <SearchContainer $showOnDesktop={showOnDesktop}>
         <Icon onClick={showSearch} />
         <Input ref={inputEl} type="text" name="search" onChange={handleChange} autoComplete="off" />
