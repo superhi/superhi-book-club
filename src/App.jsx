1d4f60cb9323962b02ad47919ba93260d1750fda
isabelxklee <hello@isabelklee.co>
2021-04-29 15:58:32 -0400

    added try/catch

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,59 @@ const App = () => {
   const [books, setBooks] = useState([])
   console.log('this message is going to load every time the component renders.')
 
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     console.log('when is this message going to load?')
+  //   }, 2000)
+
+  //   const fetchData = async () => {
+  //     const response = await fetch('https://book-club-json.herokuapp.com/books')
+  //     console.log(`here's what our fetch request returns:`, response)
+
+  //     if (response.ok) {
+  //       const books = await response.json()
+  //       console.log(`our json-ified response:`, books)
+
+  //       setBooks(books)
+  //     }
+  //   }
+
+  //   // const fetchData = () => {
+  //   //   fetch('https://book-club-json.herokuapp.com/books')
+  //   //     .then((response) => {
+  //   //       console.log(`here's what our fetch request returns:`, response)
+  //   //       return response.json()
+  //   //     })
+  //   //     .then((books) => {
+  //   //       console.log(`our json-ified response:`, books)
+  //   //       return setBooks(books)
+  //   //     })
+  //   // }
+
+  //   fetchData()
+  // }, [])
+
   useEffect(() => {
     setTimeout(() => {
       console.log('when is this message going to load?')
-    }, Math.random() * 1000)
+    }, 2000)
 
     const fetchData = async () => {
-      console.log('then after the component has rendered, this effect hook is going to run.')
-      const response = await fetch('https://book-club-json.herokuapp.com/books')
-      console.log('here is the original response from the API: ', response)
-      const books = await response.json()
-      console.log('and the jsonified books array', books)
-      setBooks(books)
+      try {
+        const response = await fetch('https://book-club-json.herokuapp.com/books')
+        console.log(`here's what our fetch request returns:`, response)
+        const books = await response.json()
+        console.log(`our json-ified response:`, books)
+        setBooks(books)
+      } catch (errors) {
+        console.log(errors)
+      }
     }
 
     fetchData()
   }, [])
 
-  console.log(`our state's books array:`, books)
+  console.log(`the books array in our state:`, books)
 
   return <>hello world</>
 }
