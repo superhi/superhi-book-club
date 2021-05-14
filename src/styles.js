4be87cd525ba2181830570b0012be2cfe04b43dd
isabelxklee <hi@kleetime.com>
2021-05-14 10:34:23 -0400

    added background color to body

--- src/styles.js
+++ src/styles.js
@@ -2,6 +2,7 @@ import styled, {createGlobalStyle} from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
   body {
+    background-color: #a7e1f8;
     font-family: 'Work Sans', sans-serif;
     margin: 0;
     padding: 0;
