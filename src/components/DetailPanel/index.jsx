9c47ad461c5f844103728f5ac08b608436062a4f
isabelxklee <hi@kleetime.com>
2021-05-25 13:42:53 -0400

    updated DetailPanel conditional statement

--- src/components/DetailPanel/index.jsx
+++ src/components/DetailPanel/index.jsx
@@ -21,7 +21,7 @@ const DetailPanel = ({book, closePanel, state}) => {
         <CloseWrapper onClick={closePanel} $state={state}>
           <Close />
         </CloseWrapper>
-        {book && state !== 'exited' && (
+        {book && (
           <>
             <Book book={book} isLarge={true} />
             <P>{book.description}</P>
